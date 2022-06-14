5bc77d37c8e6c4095be732cdd0192ad6fad57714
Marcus <vini.mark2002@gmail.com>
2022-06-13 22:18:45 -0300

    Adicionada função de edição de tarefa

--- to-do-list/src/components/Todo.js
+++ to-do-list/src/components/Todo.js
@@ -1,13 +1,30 @@
 import React, { useState } from 'react'
 import { RiCloseCircleLine } from 'react-icons/ri'
 import { TiEdit } from 'react-icons/ti'
+import TodoForm from './TodoForm'
 
-function Todo({todos, completeTodo}) {
+function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
     const [edit, setEdit] = useState({
         id: null,
         value: ''
     })
 
+    const submitUpdate = value => {
+        updateTodo(edit.id, value)
+        setEdit({
+            id: null,
+            value: ''
+        })
+    }
+
+    if ( edit.id ) {
+        return (
+            <TodoForm
+                edit={edit}
+                onSubmit={submitUpdate}
+            />
+        )
+    }
 
     return todos.map((todo, index) => (
         <div
@@ -18,8 +35,17 @@ function Todo({todos, completeTodo}) {
                 {todo.text}
             </div>
             <div className='icons'>
-                <RiCloseCircleLine />
-                <TiEdit />
+                <RiCloseCircleLine
+                    onClick={() => removeTodo(todo.id)}
+                    className='delete-icon'
+                />
+                <TiEdit
+                    onClick={() => setEdit({
+                        id: todo.id,
+                        value: todo.text
+                    })}
+                    className='edit-icon'
+                />
             </div>
         </div>
     ))
--- to-do-list/src/components/TodoList.js
+++ to-do-list/src/components/TodoList.js
@@ -13,6 +13,22 @@ function TodoList() {
         const newTodos = [todo, ...todos];
 
         setTodos(newTodos);
+    }
+
+    const updateTodo = (todoId, newValue) => {
+        if (!newValue.text || /^\s*$/.test(newValue.text)) {
+            return;
+        }
+
+        setTodos(prev => prev.map(
+            item => (item.id === todoId ? newValue : item)
+        ))
+    }
+
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id)
+
+        setTodos(removeArr)
     };
 
     const completeTodo = id => {
@@ -29,9 +45,11 @@ function TodoList() {
         <div>
             <h1>Quais os planos para hoje?</h1>
             <TodoForm onSubmit={addTodo} />
-            <Todo 
+            <Todo
                 todos={todos}
                 completeTodo={completeTodo}
+                removeTodo={removeTodo}
+                updateTodo={updateTodo}
             />
         </div>
     );
